30b19c158c45ba9404f2bc99c69f7b69e75c4375
m-ghiani <m.ghiani@gmail.com>
2023-12-17 02:05:09 +0100

    Update version number in setup.py

--- rabbit_manager_package/setup.py
+++ rabbit_manager_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gh-rabbit-hole",
-    version="1.0.0",
+    version="1.0.0.1",
     author="Massimo Ghiani",
     author_email="m.ghiani@gmail.com",
     packages=find_packages(
